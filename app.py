61ad6260a804135f58118f317b881dd302fd8777
ahmad <ahmedorfaly1982@gmail.com>
2026-03-29 19:06:55 +0100

    add logic for encryption and decryption

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
+from cryptography.fernet import Fernet
+
+# def write_key():
+#     key = Fernet.generate_key()
+#     with open("key.key", "wb") as key_file:
+#         key_file.write(key)
+# write_key()
+
+def load_key():
+    file = open("key.key","rb")
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key()
+fer = Fernet(key)
+
+
 def add():
     name = input("Account Name: ")
     pwd = input("Password: ")
     with open("passwords.txt", "a") as f:
-        f.write(name + "|" + pwd +"\n")
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() +"\n")
 
 def view():
     with open("passwords.txt", "r") as f:
         for line in f.readlines():
             data = line.strip()
             name, password = data.split("|")
-            print(f"Name: {name} | Password: {password}")
+            decrypted_password = fer.decrypt(password.encode()).decode()
+            print(f"Name: {name} | Password: {decrypted_password}")
             print("----------------------------------------------")
 
 while True:
