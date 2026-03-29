211067cf52a8d9a2eaf214c0651d4260569f5c46
ahmad <ahmedorfaly1982@gmail.com>
2026-03-29 14:14:02 +0100

    add functions for adding and viewing passwords

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
+def add():
+    pass
+def view():
+    pass
+
 while True:
     mode = input("would you like to view password or create a new one?"
-    "(view/add) or click quit to end the program: ").lower()
-    
+    "(view/add) or click quit to end the program: ").lower()  
     if mode == "quit":
         print("See you soon!")
         break
     if mode == "view":
-        print("view ")
+        view()
     elif mode == "add":
-        print("add")
+        add()
     else:
         print("Invalid mode!")
         continue
