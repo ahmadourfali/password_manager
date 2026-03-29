b23447d1cbc6aba8800b89adedac58d07f3a460d
ahmad <ahmedorfaly1982@gmail.com>
2026-03-29 14:56:06 +0100

    add logic to view function

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 def add():
     name = input("Account Name: ")
     pwd = input("Password: ")
-    with open("paswords.txt", "a") as f:
+    with open("passwords.txt", "a") as f:
         f.write(name + "|" + pwd +"\n")
 
 def view():
-    pass
+    with open("passwords.txt", "r") as f:
+        for line in f.readlines():
+            data = line.strip()
+            name, password = data.split("|")
+            print(f"Name: {name} | Password: {password}")
+            print("----------------------------------------------")
 
 while True:
     mode = input("would you like to view password or create a new one?"
